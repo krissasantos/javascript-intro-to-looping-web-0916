8a128b7f3c38152dc3a969524fc8534b0fbf27be
krissasantos <krissa.santos31@gmail.com>
2016-11-03 16:54:32 -0400

    Done.

--- loops.js
+++ loops.js
@@ -9,8 +9,8 @@ function forLoop(array) {
 
   }
   return array
-} 
- 
+}
+
 
 
 
@@ -24,7 +24,7 @@ function whileLoop(n) {
 
 
 ////////////////////
- 
+
 
 
 function doWhileLoop(array) {
@@ -32,17 +32,9 @@ function doWhileLoop(array) {
  function maybeTrue() {
   return Math.random() >= 0.5
   }
-  
+
     do {
       array = array.slice(1)
     } while (array.length > 0 && maybeTrue());
     return array
 }
-
-
-
-
-
-
-
-
