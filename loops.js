3169e5ff94bf10551689e8f06f705ea4329c10b2
krissasantos <krissa.santos31@gmail.com>
2016-11-03 15:30:06 -0400

    Done.

--- loops.js
+++ loops.js
@@ -24,12 +24,12 @@ function whileLoop(n) {
 
 
 ////////////////////
-
+ 
 
 
 function doWhileLoop(array) {
 
-  function maybeTrue() {
+ function maybeTrue() {
   return Math.random() >= 0.5
   }
   
